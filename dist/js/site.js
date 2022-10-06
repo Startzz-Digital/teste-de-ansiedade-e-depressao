10fc11abbff2470da2ff9a90f0437d6a7d157fdc
UnixProtons <101668507+unixprotons@users.noreply.github.com>
2022-10-06 18:45:59 -0300

    Initial commit

---
+++ dist/js/site.js
@@ -0,0 +1,704 @@
+
+let questions = [
+  {
+    id: 1,
+    question: "Fiquei chateado com coisas bastante triviais nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 2,
+    question: "Eu estava ciente da secura da minha boca nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 3,
+    question: "Eu não conseguia sentir nenhum sentimento positivo nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 4,
+    question: "Senti dificuldade em respirar (por exemplo, respiração excessivamente rápida, falta de ar na ausência de esforço físico) nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 5,
+    question: "Você sentuu simplesmente não conseguia seguir em frente nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 6,
+    question: "Eu tendia a reagir exageradamente às situações nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 7,
+    question: "Tive uma sensação de tremor (por exemplo, parece que as pernas vão ceder), nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 8,
+    question: "Achei difícil relaxar nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 9,
+    question: "Encontrei-me em situações que me deixaram tão ansioso que fiquei mais aliviado quando terminaramnas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+  {
+    id: 10,
+    question: "Senti que não tinha nada para esperar nas últimas duas semanas.",
+    pointLow: "Nada Haver Comigo",
+    pointMid:"Alguns Dias",
+    pointHigh:"Mais da metade dos dias",
+    pointVeryHigh:"Quase todos os dias",
+
+    options: [
+      "Nada Haver Comigo",
+      "Mais da metade dos dias",
+      "Alguns Dias",
+      "Quase todos os dias"
+    ]
+  },
+{
+id: 11,
+  question: "Eu me peguei ficando chateado facilmente nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 12,
+  question: "Senti que estava usando muita energia nervosa nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 13,
+  question: "Senti-me triste e deprimido nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 14,
+  question: "Fiquei impaciente quando me atrasei, quando fiquei esperando de qualquer maneira (por exemplo, elevadores, semáforos, ficar esperando nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 15,
+  question: "Tive uma sensação de desmaio nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 16,
+  question: "Senti que havia perdido o interesse em quase tudo nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 17,
+  question: "Senti que não valia muito como pessoa nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 18,
+  question: "Senti que estava bastante sensível nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 19,
+  question: "Transpirava visivelmente (por exemplo, mãos suadas) na ausência de altas temperaturas ou esforço físico nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 20,
+  question: "Eu me senti assustado sem nenhuma boa razão nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 21,
+  question: "Senti que a vida não valia a pena nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 22,
+  question: "Achei difícil relaxar nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 23,
+  question: "Tive dificuldade em engolir alimentos normalmente nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 24,
+  question: "Eu não conseguia tirar nenhum prazer das coisas que fazia nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 25,
+  question: "Eu estava ciente da ação do meu coração na ausência de esforço físico (por exemplo, sensação de aumento da frequência cardíaca, coração perdendo uma batida) nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 26,
+  question: "Senti-me desanimado e triste nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 27,
+  question: "Achei que estava muito irritado nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 28,
+  question: "Senti que estava perto do pânico nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 29,
+  question: "Achei difícil me acalmar depois que algo me aborreceu  nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 30,
+  question: "Eu temia ser surpreendido por alguma tarefa trivial, mas desconhecida nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 31,
+  question: "Não consegui me entusiasmar com nada nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 32,
+  question: "Achei difícil tolerar interrupções no que estava fazendo nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 33,
+  question: "Eu estava em estado de tensão nervosa nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 34,
+  question: "Senti que não valia nada nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 35,
+  question: "Eu era intolerante com qualquer coisa que me impedisse de continuar com o que estava fazendo nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 36,
+  question: "Fiquei apavorado nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 37,
+  question: "Eu não conseguia ver nada no futuro para ter esperança nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 38,
+  question: "Senti que a vida não tinha sentido nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 39,
+  question: "Fiquei agitado nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 40,
+  question: "Eu estava preocupado com situações em que eu poderia entrar em pânico e fazer papel de bobo nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 41,
+  question: "Senti tremores (por exemplo, nas mãos) nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+{
+id: 42,
+  question: "Achei difícil desenvolver a iniciativa de fazer as coisas nas últimas duas semanas.",
+  pointLow: "Nada Haver Comigo",
+  pointMid:"Alguns Dias",
+  pointHigh:"Mais da metade dos dias",
+  pointVeryHigh:"Quase todos os dias",
+
+  options: [
+    "Nada Haver Comigo",
+    "Mais da metade dos dias",
+    "Alguns Dias",
+    "Quase todos os dias"
+  ]
+},
+
+];
+
+let question_count = 0;
+let points = 0;
+
+window.onload = function() {
+  show(question_count);
+
+};
+
+function next() {
+
+
+  // if the question is last then redirect to final page
+  if (question_count == questions.length - 1) {
+    sessionStorage.setItem("time", time);
+    clearInterval(mytime);
+    location.href = "resultado.html";
+  }
+  console.log(question_count);
+
+  let user_answer = document.querySelector("li.option.active").innerHTML;
+  // check if the answer is right or wrong
+  if (user_answer == questions[question_count].pointLow) {
+    points += 0;
+    sessionStorage.setItem("points", points);
+  }
+  else if (user_answer == questions[question_count].pointMid){
+    points += 1;
+    sessionStorage.setItem("points", points);
+  }
+  else if (user_answer == questions[question_count].pointHigh){
+    points += 2;
+    sessionStorage.setItem("points", points);
+  }
+  else if (user_answer == questions[question_count].pointVeryHigh){
+    points += 3;
+    sessionStorage.setItem("points", points);
+  }
+  console.log(points);
+
+  question_count++;
+  show(question_count);
+}
+
+function show(count) {
+  let question = document.getElementById("questions");
+  let [first, second, third, fourth] = questions[count].options;
+
+  question.innerHTML = `
+  <h2>Q${count + 1}. ${questions[count].question}</h2>
+   <ul class="option_group">
+  <li class="option">${first}</li>
+  <li class="option">${second}</li>
+  <li class="option">${third}</li>
+  <li class="option">${fourth}</li>
+</ul>
+  `;
+  toggleActive();
+}
+
+function toggleActive() {
+  let option = document.querySelectorAll("li.option");
+  for (let i = 0; i < option.length; i++) {
+    option[i].onclick = function() {
+      for (let i = 0; i < option.length; i++) {
+        if (option[i].classList.contains("active")) {
+          option[i].classList.remove("active");
+        }
+      }
+      option[i].classList.add("active");
+    };
+  }
+}
